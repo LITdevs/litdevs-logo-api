991b20f2940f9f6ee1fa1fde756791ad53f5a1f9
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-06-13 15:49:14 +0300

    actually working version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function initAPIs(utils) {
     client = utils.client
     setInterval(() => {
         fetch("https://logo.litdevs.org/api/vukky/bg").then(res => res.text()).then(res => {
-            sharp(res).toBuffer.then(jpeg => {
+            sharp(Buffer.from(res)).toBuffer().then(jpeg => {
                 client.guilds.fetch("868937321402204220").then(server => server.setIcon(jpeg).catch(err => {
                     console.log(err)
                 }))
