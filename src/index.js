8f52513908851c7df6809aec8129657f96aa29b2
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-06-13 14:05:20 +0300

    bug fixes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const { convert } = require('convert-svg-to-jpeg');
 function initAPIs(utils) {
     client = utils.client
     setInterval(() => {
-        fetch("https://logo.litdevs.org/api/vukky/bg").then(res => res.text).then(res => {
+        fetch("https://logo.litdevs.org/api/vukky/bg").then(res => res.text()).then(res => {
             convert(res).then(jpeg => {
-                client.guilds.fetch("868937321402204220").setIcon(jpeg).catch(err => {
+                client.guilds.fetch("868937321402204220").then(server => server.setIcon(jpeg).catch(err => {
                     console.log(err)
-                })
+                }))
             })
         })
-    }, 120000)
+    }, 300000)
 }
 
 module.exports = {
